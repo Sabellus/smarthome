9483de58590548aca681575a0a8b3e18d3ac4954
Савелий Вепрев <veprev@MacBook-Pro-Savelij.local>
2019-05-26 03:22:55 +0500

    Initial Commit

---
+++ SmartHome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartHome
+//
+//  Created by Савелий Вепрев on 26/05/2019.
+//  Copyright © 2019 Савелий Вепрев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
